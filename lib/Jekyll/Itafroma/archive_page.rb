e8d780515e33475bfa014d30ada9fd3c779e3aa7
Mark Trapp <mark@marktrapp.com>
2014-02-13 00:55:33 -0800

    Refactor ArchivePage to avoid pollution.
    Fixes #4.

--- lib/Jekyll/Itafroma/archive_page.rb
+++ lib/Jekyll/Itafroma/archive_page.rb
@@ -16,40 +16,50 @@ module Jekyll
     # Represents an archive listing page.
 
     class ArchivePage < Page
-      def initialize(site, base, pattern, posts)
+      def initialize(site, base, date_pattern, posts)
         @site = site
-        @base = base
-        @dir = posts.first.date.strftime(pattern)
-        @name = 'index.html'
         @config = site.config['archive']
+        @posts = posts
+
+        @date_pattern = date_pattern
+        @base = base
+        @dir = dir
+        @name = 'index.html'
 
         process(@name)
 
         layout = @config['layout'] || 'archive'
         read_yaml(File.join(base, '_layouts'), "#{layout}.html")
 
-        data['archive'] = {
-          'date_pattern' => pattern,
-          'posts' => posts
-        }
-        data['date'] = posts.first.date
-        data['title'] = title
+        populate_data!
       end
 
-      def url
-        base = site.config['archive']['path'] || '/'
-        File.join(base, @dir, 'index.html')
+      # Returns a +Date+ containing the archive's start date.
+      # To determine the archive's start date, the date of the first post is
+      # converted to a string using `@date_pattern`. Doing this will clear any
+      # precision beyond what's specified in `@date_pattern`.
+      def date
+        date_string = @posts.first.date.strftime(@date_pattern)
+        DateTime.strptime(date_string, @date_pattern)
       end
 
       def title
         tokenized_title = @config['title'] || 'Blog archive - :date'
-        date = data['date']
-        pattern = data['archive']['date_pattern']
-        tokenized_title.gsub(':date', date.strftime(pattern))
+        tokenized_title.gsub(':date', date.strftime(@date_pattern))
       end
 
-      def to_liquid
-        data.deep_merge('url' => url, 'content' => content)
+      def url
+        base = @config['path'] || '/'
+        File.join(base, date.strftime(@date_pattern), 'index.html')
+      end
+
+      def populate_data!
+        data.merge!('date' => date,
+                    'title' => title,
+                    'archive' => {
+                      'date_pattern' => @date_pattern,
+                      'posts' => @posts
+                    })
       end
     end
   end
